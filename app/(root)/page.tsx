22ef0c38eecc15df3233625d0199848f38034bfb
Sacchit Wathe <sacchit.wathe06@gmail.com>
2024-06-05 15:33:03 +0530

    implement chart js function and the home card for user display

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,29 @@
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = { firstName: "Sacchit" };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "guest"}
+            subtext="Access and manage your accounts and transactions efficiently and securely."
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={20023.45}
+          />
+        </header>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
