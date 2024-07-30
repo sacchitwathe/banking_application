1c1d3eda3a807f236be00f911ef5f1a41947ead3
Sacchit Wathe <sacchit.wathe06@gmail.com>
2024-07-31 02:01:22 +0530

    Implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,14 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  const loggedIn = { firstName: "Sacchit" };
+  const loggedIn = {
+    firstName: "Sacchit ",
+    lastName: "Wathe",
+    email: "sacchit.wathe@gmail.com",
+  };
   return (
     <section className="home">
       <div className="home-content">
@@ -21,7 +26,14 @@ const Home = () => {
             totalCurrentBalance={20023.45}
           />
         </header>
+        Recent transactions
       </div>
+
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 23000.43 }, { currentBalance: 45000.45 }]}
+      />
     </section>
   );
 };
